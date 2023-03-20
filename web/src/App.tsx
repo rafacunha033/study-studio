0896755f77fc5e231d0eda1f71da9c5c947a8bb9
rafacunha033 <rafacunha.prof033@gmail.com>
2023-03-20 15:16:27 -0300

    Vite+Tailwind

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+import { Header } from "./components/Header";
+
+
+function App() {
+  return (
+    <div className='bg-slate-800'>
+      <Header />
+      <h1 className='text-white'>Hello there</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+
+export function Header() {
+
+    return (
+        <div className="text-white before:w-full max-w-3xl mx-auto flex items-center justify-between">
+            Header 
+        </div>
+    )
+}
